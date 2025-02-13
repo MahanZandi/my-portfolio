52e542b636ffbd0bab1acbe127fc5116c23fd330
mahan <zandim221@gmail.com>
2025-02-13 17:46:29 +0330

    created link scss component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ const App = () => {
     <div dir="rtl" className="bg-gray-200 text-gray-600 dark:bg-gray-900 h-[100vh] dark:text-gray-400 transition-all">
       <div>
         <ThemeToggle/>
+        <p className="p-10 text-2xl link link-underline">
+          test
+        </p>
       </div>
     </div>
   )
