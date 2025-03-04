d130e5eaa333164da2244a44ce24aa393ff8878f
mahan <zandim221@gmail.com>
2025-03-04 13:34:02 +0330

    copy actions done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,3 +1,6 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { FiCopy } from "react-icons/fi";
 import { FiPhone } from "react-icons/fi";
@@ -5,11 +8,21 @@ import { TbBrandGithub } from "react-icons/tb";
 import { LiaTelegramPlane } from "react-icons/lia";
 import { FaInstagram } from "react-icons/fa";
 import { PiLinkedinLogoBold } from "react-icons/pi";
-import Link from "next/link";
+import { LuCopyCheck } from "react-icons/lu";
 
 const Contact = () => {
+  const [copied, setCopied] = useState<"email" | "phone" | null>(null);
+
+  const handleCopy = (type: "email" | "phone") => {
+    const text = type === "email" ? "zandim221@gmail.com" : "+98 9395526996";
+    navigator.clipboard.writeText(text).then(() => {
+      setCopied(type);
+      setTimeout(() => setCopied(null), 2000); // Reset after 2 seconds
+    });
+  };
+
   return (
-    <div className="contact">
+    <div className="contact" data-first-enter-image="true">
       {/* title */}
       <div className="contact-tags-container">
         <span className="tags">تماس بگیرید</span>
@@ -24,8 +37,11 @@ const Contact = () => {
       {/* email & phone number */}
       <div className="contact-content">
         <div className="contact-info">
-          <span className="contact-info-icon-copy">
-            <FiCopy />
+          <span
+            className="contact-info-icon-copy"
+            onClick={() => handleCopy("email")}
+          >
+            {copied === "email" ? <LuCopyCheck/> : <FiCopy />}
           </span>
           <span className="contact-info-text">zandim221@gmail.com</span>
           <span className="contact-info-icon">
@@ -34,8 +50,11 @@ const Contact = () => {
         </div>
 
         <div className="contact-info">
-          <span className="contact-info-icon-copy">
-            <FiCopy />
+          <span
+            className="contact-info-icon-copy"
+            onClick={() => handleCopy("phone")}
+          >
+            {copied === "phone" ? <LuCopyCheck/> : <FiCopy />}
           </span>
           <span dir="ltr" className="contact-info-text">
             +98 9395526996
@@ -50,19 +69,19 @@ const Contact = () => {
         <span className="contact-social-title">
           شما همچنین میتوانید با من در این فضا ها ارتباط بگیرید:
         </span>
-          <div className="contact-social-links">
-            <Link href="#" className="actions">
-              <TbBrandGithub />
-            </Link>
-            <Link href="#" className="actions">
-              <LiaTelegramPlane />
-            </Link>
-            <Link href="#" className="actions">
-              <FaInstagram />
-            </Link>
-            <Link href="#" className="actions">
-              <PiLinkedinLogoBold />
-            </Link>
+        <div className="contact-social-links">
+          <Link href="#" className="actions">
+            <TbBrandGithub />
+          </Link>
+          <Link href="#" className="actions">
+            <LiaTelegramPlane />
+          </Link>
+          <Link href="#" className="actions">
+            <FaInstagram />
+          </Link>
+          <Link href="#" className="actions">
+            <PiLinkedinLogoBold />
+          </Link>
         </div>
       </div>
     </div>
