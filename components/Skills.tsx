7a968d1a1730da98a20c65945711d79a2d2a7cbd
mahan <zandim221@gmail.com>
2025-04-21 23:48:13 +0330

    added three js in website

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,14 @@
 import HtmlIcon from "@/public/images/skills-icons/icons8-html.svg";
 import CssIcon from "@/public/images/skills-icons/icons8-css.svg";
 import TailwindIcon from "@/public/images/skills-icons/icon-tailwindcss.svg";
-import ScssIcon from "@/public/images/skills-icons/icon-sass.svg"
+import ScssIcon from "@/public/images/skills-icons/icon-sass.svg";
 import JsIcon from "@/public/images/skills-icons/icon-javscript.svg";
 import TsIcon from "@/public/images/skills-icons/icon-typescript.svg";
 import ReactIcon from "@/public/images/skills-icons/icon-react.svg";
 import NextJsIcon from "@/public/images/skills-icons/icon-nextjs.svg";
 import GitIcon from "@/public/images/skills-icons/icon-git.svg";
+import ThreejsIcon from "@/public/images/skills-icons/Threejs-logo.svg";
+import Link from "next/link";
 
 import Image from "next/image";
 
@@ -26,109 +28,121 @@ const Skills: React.FC = () => {
           <div className="skills-content-container">
             <div>
               <div className="skills-item">
-                <a href="https://www.w3schools.com/html/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={HtmlIcon} 
-                      alt="html icon" 
-                    />
-                </a>
+                <Link href="https://www.w3schools.com/html/">
+                  <Image
+                    className="skills-item-image"
+                    src={HtmlIcon}
+                    alt="html icon"
+                  />
+                </Link>
                 <p className="skills-item-text">HTML</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://www.w3schools.com/css/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={CssIcon} 
-                      alt="css icon" 
-                    />
-                </a>
+                <Link href="https://www.w3schools.com/css/">
+                  <Image
+                    className="skills-item-image"
+                    src={CssIcon}
+                    alt="css icon"
+                  />
+                </Link>
                 <p className="skills-item-text">CSS</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://tailwindcss.com/">
-                    <Image
-                      className="skills-item-image"
-                      src={TailwindIcon}
-                      alt="Tailwind css icon"
-                    />
-                </a>
+                <Link href="https://tailwindcss.com/">
+                  <Image
+                    className="skills-item-image"
+                    src={TailwindIcon}
+                    alt="Tailwind css icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Tailwind css</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://sass-lang.com/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={ScssIcon} 
-                      alt="sass/scss icon" 
-                    />
-                </a>
+                <Link href="https://sass-lang.com/">
+                  <Image
+                    className="skills-item-image"
+                    src={ScssIcon}
+                    alt="sass/scss icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Sass/Scss</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-                    <Image
-                      className="skills-item-image"
-                      src={JsIcon}
-                      alt="Javascript icon"
-                    />
-                </a>
+                <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                  <Image
+                    className="skills-item-image"
+                    src={JsIcon}
+                    alt="Javascript icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Javascript</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://www.typescriptlang.org/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={TsIcon} 
-                      alt="Typescript icon" 
-                    />
-                </a>
+                <Link href="https://www.typescriptlang.org/">
+                  <Image
+                    className="skills-item-image"
+                    src={TsIcon}
+                    alt="Typescript icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Typescript</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://react.dev/">
-                    <Image 
-                    className="skills-item-image" 
-                    src={ReactIcon} 
-                    alt="react icon" 
-                    />
-                </a>
+                <Link href="https://react.dev/">
+                  <Image
+                    className="skills-item-image"
+                    src={ReactIcon}
+                    alt="react icon"
+                  />
+                </Link>
                 <p className="skills-item-text">React</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://nextjs.org/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={NextJsIcon} 
-                      alt="Next js icon" 
-                    />
-                </a>
+                <Link href="https://nextjs.org/">
+                  <Image
+                    className="skills-item-image"
+                    src={NextJsIcon}
+                    alt="Next js icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Next Js</p>
               </div>
             </div>
             <div>
               <div className="skills-item">
-                <a href="https://git-scm.com/">
-                    <Image 
-                      className="skills-item-image" 
-                      src={GitIcon} 
-                      alt="git icon" 
-                    />
-                </a>
+                <Link href="https://threejs.org/">
+                  <Image
+                    className="skills-item-image"
+                    src={ThreejsIcon}
+                    alt="git icon"
+                  />
+                </Link>
+                <p className="skills-item-text">Three js</p>
+              </div>
+            </div>
+            <div>
+              <div className="skills-item">
+                <Link href="https://git-scm.com/">
+                  <Image
+                    className="skills-item-image"
+                    src={GitIcon}
+                    alt="git icon"
+                  />
+                </Link>
                 <p className="skills-item-text">Git</p>
               </div>
             </div>
