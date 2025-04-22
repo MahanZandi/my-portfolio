722f887d83c320a2c0fc68d033e0f086e2e3b694
mahan <zandim221@gmail.com>
2025-04-22 18:19:14 +0330

    fixed side bar download button

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,59 +3,70 @@ import { RxCross2 } from "react-icons/rx";
 import ThemeToggle from "./ThemeTogle";
 import Button from "./Button";
 
-const Sidebar = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: (open: boolean) => void }) => {
-    return (
-      <div>
-        {isOpen && (
-          <div className="sidebar-overlay" onClick={() => setIsOpen(false)}></div>
-        )}
-        
-        <div
-          className={`sidebar ${
-            isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-        >
-          <div className="sidebar-top">
-            <button 
-                  onClick={() => setIsOpen(false)} 
-                  className="sidebar-close-btn"
-                  >
-                <RxCross2/>
-            </button>
-            <Link href="/" className="sidebar-logo">
-              {"</ M>"}
+const Sidebar = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}) => {
+  return (
+    <div>
+      {isOpen && (
+        <div className="sidebar-overlay" onClick={() => setIsOpen(false)}></div>
+      )}
+
+      <div
+        className={`sidebar ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="sidebar-top">
+          <button
+            onClick={() => setIsOpen(false)}
+            className="sidebar-close-btn"
+          >
+            <RxCross2 />
+          </button>
+          <Link href="/" className="sidebar-logo">
+            {"</ M>"}
+          </Link>
+        </div>
+        <div className="sidebar-top-hr-parents">
+          <hr className="sidebar-top-hr"></hr>
+        </div>
+        <nav className="sidebar-items-container">
+          <span onClick={() => setIsOpen(false)}>
+            <Link className="link" href="#about">
+              درباره من
             </Link>
+          </span>
+          <span onClick={() => setIsOpen(false)}>
+            <Link className="link" href="#works">
+              نمونه کارهام
+            </Link>
+          </span>
+          <span onClick={() => setIsOpen(false)}>
+            <Link className="link" href="#contact">
+              تماس با من
+            </Link>
+          </span>
+        </nav>
+        <div className="sidebar-button-hr-parents">
+          <hr className="sidebar-button-hr"></hr>
+        </div>
+        <div className="sidebar-tools-container">
+          <div className="sidebar-theme-toggle-container">
+            <p className="sidebar-switch-theme-text">تعویض تم</p>
+            <ThemeToggle className="sidebar-theme-toggle" />
           </div>
-          <div className="sidebar-top-hr-parents">
-            <hr className="sidebar-top-hr"></hr>
-          </div>
-          <nav className="sidebar-items-container">
-            <span>
-              <Link className="link" href="#about">درباره من</Link>
-            </span>
-            <span>
-              <Link className="link" href="#works">نمونه کارهام</Link>
-            </span>
-            <span>
-              <Link className="link" href="#contact">تماس با من</Link>
-            </span>
-          </nav>
-          <div className="sidebar-button-hr-parents">
-            <hr className="sidebar-button-hr"></hr>
-          </div>
-          <div className="sidebar-tools-container">
-              <div className="sidebar-theme-toggle-container"> 
-                <p className="sidebar-switch-theme-text">تعویض تم</p>
-                <ThemeToggle className="sidebar-theme-toggle"/>
-              </div>
-              <div>
-                <Button className="button-sidebar" text="دانلود رزومه"/>
-              </div>
+          <div onClick={() => setIsOpen(false)}>
+            <a href="/files/Mahan-resume.pdf" target="_blank">
+              <Button className="button-sidebar" text="دانلود رزومه" />
+            </a>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default Sidebar;
-  
+    </div>
+  );
+};
+
+export default Sidebar;
